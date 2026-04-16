7f7e43df234ca7f1a0d4b7a61864e003549f2474
keitharwil <keith110606@gmail.com>
2026-04-16 16:20:05 +0800

    COMPLETED 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let message = '';
+    let loops = num
+    if (loops < 0){
+        return "ERROR";
+        
+    } else {
+        for(; loops > 0; loops--){
+            message += string;
+        };
+        return message;
+    };
 };
 
 // Do not edit below this line
