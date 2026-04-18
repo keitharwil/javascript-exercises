54d105ceee8b1cea956d0f1023f8e5cc6ee48a34
keitharwil <keith110606@gmail.com>
2026-04-19 00:49:13 +0800

    Completed 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return reversedString = string.split("").reverse().join("");
 };
 
 // Do not edit below this line
