1e0f446b241a2bdb8872db7bfebfbf41971aba38
keitharwil <keith110606@gmail.com>
2026-04-19 04:09:41 +0800

    Completed 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if (!Number.isInteger(firstNum)||!Number.isInteger(secondNum)|| firstNum < 0 || secondNum < 0){
+        return "ERROR"
+    };
+    const calculate = (lowerNum, biggerNum) => {
+        let sum = 0;
+        let adding = lowerNum;
+        for(let i = lowerNum; i <= biggerNum; i++){sum += adding++;};
+        return sum;
+    };
+    switch (true){
+        case firstNum < secondNum:
+            return calculate(firstNum, secondNum);
+        case secondNum < firstNum:
+            return calculate(secondNum, firstNum);
+        case firstNum == secondNum:
+            return firstNum;
+        default:
+            return "ERROR";
+    };
+    
 };
 
 // Do not edit below this line
