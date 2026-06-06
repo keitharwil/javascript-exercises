821d121e13977c236ccc88a5fdf439873f677252
keithkat <keith110606@gmail.com>
2026-06-06 22:40:52 +0800

    Completed 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function(numbers) {
+	return numbers.reduce((a, b) => a + b, 0);
 
 };
 
-const power = function() {
-	
+const multiply = function(numbers) {
+	return numbers.reduce((a, b) => a * b);
 };
 
-const factorial = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
+};
+
+const factorial = function(num) {
+  const numbers = new Array();
+  for (let i = num; i >= 1; i--) {
+    numbers.push(i);
+  }
+  return num === 0 ?  1 : numbers.reduce((a, b) => a * b);
 };
 
 // Do not edit below this line
