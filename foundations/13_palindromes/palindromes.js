58fac346a4f9d685d919e14a83092f1461b1eeac
keitharwil <keith110606@gmail.com>
2026-06-22 01:23:34 +0800

    completed palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const punctuations = [
+        '.', ',', ';', ':', '!', '?', '"', "'", '`', '-', 
+        '–', '—', '(', ')', '[', ']', '{', '}', '⟨', '⟩', 
+        '…', '/', '\\', '|', '@', '&', '*', '_', '§', '¶', 
+        '†', '‡', '•', '°', '¡', '¿', '«', '»', '‹', '›'
+    ];
+    const isPalindrome = input
+                            .toLowerCase()
+                            .replaceAll(" ", "")
+                            .split("")
+                            .toReversed()
+                            .filter((value) => !punctuations.includes(value))
+                            .join("");
 
+   return isPalindrome === isPalindrome.split("").toReversed().join("") ? true : false
 };
 
 // Do not edit below this line
