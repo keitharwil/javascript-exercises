a8e881e69ae7f8a90e19d5e1935c2c68b3ced722
keithkat <keith110606@gmail.com>
2026-06-22 22:43:19 +0800

    Completed 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let previous = 0;
+    let current = 1;
+    let fMember = n
+    let next;
 
+    if (typeof n !== "number") fMember = parseInt(fMember);
+    if (fMember > 0){
+        for (let i = 1; i < fMember; i++){
+            next = previous + current;
+            previous = current;
+            current = next;
+            next = 0;
+        };
+        return current;
+    } else if (fMember < 0){
+        return "OOPS";
+    } else {
+        return 0;
+    }; 
 };
 
 // Do not edit below this line
